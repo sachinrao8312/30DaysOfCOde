ecdfaf14059def240d1a6bff6292d3d02e35ca19
Sachin Rao <biz.sachinrao@gmail.com>
2023-06-30 21:26:45 +0530

    First Day

---
+++ day0.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class day0 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input_String = scanner.nextLine();
+
+        scanner.close();
+        System.out.println("Hello, World!");
+
+        System.out.println(input_String);
+    }
+}
